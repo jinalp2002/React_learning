f0cecef3c771fa47d4e70a75f86cc0999378f9ea
Jinal <jpatel111102@gmail.com>
2026-02-25 19:07:56 +0530

    passwordchanger

--- 05_passwordgenerator/src/App.jsx
+++ 05_passwordgenerator/src/App.jsx
@@ -1,6 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
